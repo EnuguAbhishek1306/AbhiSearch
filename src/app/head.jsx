c782f60d862e7c59c56543ba94fc5fdc29a54059
Abhishek reddy <abhishekreddy1306@gmail.com>
2023-03-05 11:13:45 +0530

    added favicon

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 export default function Head() {
   return (
@@ -7,6 +7,7 @@ export default function Head() {
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <meta name="description" content="AbhiSearch" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.svg" />
     </>
-  )
+  );
 }
