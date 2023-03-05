db971ef1368911913a2a4ac1e88462ba1477bf8e
Abhishek reddy <abhishekreddy1306@gmail.com>
2023-03-05 10:44:17 +0530

    install next js and create the first template

---
+++ src/app/head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+      <title>AbhiSearch</title>
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="description" content="AbhiSearch" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </>
+  )
+}
